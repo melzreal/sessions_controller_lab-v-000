c689f28c20310f6965a5ece6b66ba188e19c776e
Melissa Real <melissa.real@hotmail.com>
2019-02-10 16:45:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:name]
-      redirect_to '/login'
+      redirect_to '/new'
     else
       session[:name] = params[:name]
       redirect_to '/'
